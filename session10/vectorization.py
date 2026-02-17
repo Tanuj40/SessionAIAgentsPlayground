f8eda788682e1d6e2f82eb6c6d596cec99682f57
Tanuj40 <Tanujchaudhary40@gmail.com>
2026-02-17 20:45:44 +0530

    Array mult

---
+++ session10/vectorization.py
@@ -0,0 +1,22 @@
+numbers = [1, 2, 3, 4, 5]
+# out [2, 4, 6, 8]
+
+squared = []
+for i in numbers:
+    squared.append(i * 2)
+    
+    # print(squared)
+    
+import numpy as np
+
+numbers_np = np.array([1, 2, 3, 4])
+squared_np = numbers_np * 2
+print(squared_np)
+
+# Multiply [10, 20, 30] by 5
+arr = np.array([10, 20, 30])
+result = arr * 5
+print(result)
+
+
+
