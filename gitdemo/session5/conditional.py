a1879412f5854cb2c8e9dd346134187f46593358
Tanuj40 <Tanujchaudhary40@gmail.com>
2026-02-17 20:35:30 +0530

    Revision

---
+++ gitdemo/session5/conditional.py
@@ -0,0 +1,37 @@
+marks = 50
+another_marks = 30
+
+
+if another_marks >= 40:
+    print("Student has passed")
+else:
+    print("Student has failed")
+  
+  
+  
+temperature = 30
+if temperature > 25:
+  print("It is warm outside")
+else:
+    print("It is not warm outside")
+    
+    
+    
+age = 16
+
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
+    
+score = 76
+# score >= 90 print A
+# if score >= 75 then print B
+# else print C
+
+if score >= 90:
+    print("Grade A")
+elif score >= 75:
+    print("Grade B")
+else:
+    print("Grade C")
